8f2d32f07a391f56536f101cfeee4b1dc554b635
rjt <rjt.rockx@gmail.com>
2018-05-30 02:06:04 +0530

    Support for adding CurrencyTransactions
    Requires User ID, Amount and Reason. UTC Timestamp and ID is autocalculated and incremented.

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express')
 const jwt = require("jsonwebtoken");
 const config = require("./config.json");
 const sqlite_connector = require("./helpers/sqlite_connector");
-const app = express()
-const assert = require("assert");
+const app = express();
 const request = require('request');
 
 app.get('/getbal/:token', (req, res) => {
     try {
-        var j = jwt.verify(req.params.token, config.jwt_secret);
-        var uid = j.uid;
-        var tI = j.timeIssued;
+        let j = verify(req.params.token, jwt_secret);
+        let uid = j.uid;
+        let tI = j.timeIssued;
         if (tI < (Date.now() - (1000 * 5))) return res.end("{\"error\": \"Your token was not valid\"}");
         sqlite_connector.getBal(uid)
             .then((bal) => {
@@ -24,11 +23,29 @@ app.get('/getbal/:token', (req, res) => {
 })
 app.get('/setbal/:token', (req, res) => {
     try {
-        var j = jwt.verify(req.params.token, config.jwt_secret);
-        var uid = j.uid;
-        var tI = j.timeIssued;
+        let j = verify(req.params.token, jwt_secret);
+        let uid = j.uid;
+        let tI = j.timeIssued;
         if (tI < (Date.now() - (1000 * 5))) return res.end("{\"error\": \"Your token was not valid\"}");
         sqlite_connector.updateBal(uid, j.amount)
+            .then(() => {
+                res.end(JSON.stringify({
+                    success: true
+                }))
+            })
+    } catch (e) {
+        res.end("{\"error\": \"Your token was not valid\"}")
+    }
+})
+app.get('/addtransaction/:token', (req, res) => {
+    try {
+        let j = verify(req.params.token, jwt_secret);
+        let uid = j.uid;
+        let amount = j.amount;
+        let reason = j.reason;
+        let tI = j.timeIssued;
+        if (tI < (Date.now() - (1000 * 5))) return res.end("{\"error\": \"Your token was not valid\"}");
+        sqlite_connector.addTransaction(uid,amount,reason)
             .then(() => {
                 res.end(JSON.stringify({
                     success: true
