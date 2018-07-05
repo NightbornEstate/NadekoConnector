398d202241d07e0909804aee0525a2581b68ff31
rjt <rjt.rockx@gmail.com>
2018-07-05 14:43:36 +0530

    Code cleanup, Add updateguildxp
    Success is now indicated for all responses, incoming requests must have only keys required for that endpoint and nothing else.
    Error message is now slightly contextual.

--- helpers/sqlite_connector.js
+++ helpers/sqlite_connector.js
@@ -15,12 +15,12 @@ module.exports.getBal = function (uid) {
             db.get("SELECT * FROM DiscordUser WHERE UserId = $uid", {
                 $uid: uid
             }, function (err, row) {
-                resolve(row.CurrencyAmount)
-            })
-        })
+                resolve(row.CurrencyAmount);
+            });
+        });
         db.close();
-    })
-}
+    });
+};
 
 module.exports.updateBal = function (uid, amount) {
     return new Promise(function (resolve, reject) {
@@ -30,19 +30,19 @@ module.exports.updateBal = function (uid, amount) {
             }
         });
         db.serialize(() => {
-            db.run("UPDATE DiscordUser SET CurrencyAmount = ? WHERE UserId = ?2", {
-                2: uid,
-                1: amount
-            })
-            resolve()
-        })
+            db.run("UPDATE DiscordUser SET CurrencyAmount = $amount WHERE UserId = $uid", {
+                $uid: uid,
+                $amount: amount
+            });
+            resolve();
+        });
         db.close();
-    })
-}
+    });
+};
 
 module.exports.addTransaction = function (uid, amount, reason) {
     let now = new Date();
-    let date = now.getUTCFullYear() + '-' + pad(now.getUTCMonth() + 1) + '-' + pad(now.getUTCDate()) + ' ' + pad(now.getUTCHours()) + ':' + pad(now.getUTCMinutes()) + ':' + pad(now.getUTCSeconds()+ '.' + pad(now.getUTCMilliseconds()));
+    let date = now.getUTCFullYear() + '-' + pad(now.getUTCMonth() + 1) + '-' + pad(now.getUTCDate()) + ' ' + pad(now.getUTCHours()) + ':' + pad(now.getUTCMinutes()) + ':' + pad(now.getUTCSeconds() + '.' + pad(now.getUTCMilliseconds()));
     return new Promise(function (resolve, reject) {
         let db = new sqlite3.Database(path.join(config.nadeko_db_path), (err) => {
             if (err) {
@@ -50,17 +50,17 @@ module.exports.addTransaction = function (uid, amount, reason) {
             }
         });
         db.serialize(() => {
-            db.run("INSERT INTO CurrencyTransactions values((SELECT MAX(a.Id) FROM CurrencyTransactions a)+1, ?1, ?2, ?3, ?4)", {
-                4: date,
-                3: uid,
-                2: reason,
-                1: amount
-            })
-            resolve()
-        })
+            db.run("INSERT INTO CurrencyTransactions values((SELECT MAX(a.Id) FROM CurrencyTransactions a)+1, $amount, $reason, $uid, $date)", {
+                $date: date,
+                $uid: uid,
+                $reason: reason,
+                $amount: amount
+            });
+            resolve();
+        });
         db.close();
-    })
-}
+    });
+};
 
 module.exports.getGuildXp = function (uid, guildId) {
     return new Promise(function (resolve, reject) {
@@ -74,9 +74,28 @@ module.exports.getGuildXp = function (uid, guildId) {
                 $uid: uid,
                 $guildid: guildId
             }, function (err, row) {
-                resolve([row.Xp, row.AwardedXp])
-            })
-        })
+                resolve([row.Xp, row.AwardedXp]);
+            });
+        });
         db.close();
-    })
-}
+    });
+};
+
+module.exports.updateGuildXp = function (uid, guildId, xp) {
+    return new Promise(function (resolve, reject) {
+        let db = new sqlite3.Database(path.join(config.nadeko_db_path), (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+        db.serialize(() => {
+            db.run("UPDATE UserXpStats SET AwardedXp = $xp WHERE UserId = $uid AND GuildId = $guildid", {
+                $guildid: guildId,
+                $uid: uid,
+                $xp: xp
+            });
+            resolve();
+        });
+        db.close();
+    });
+};
--- server.js
+++ server.js
@@ -6,68 +6,136 @@ var sqlite_connector = require("./helpers/sqlite_connector");
 
 app.use('/getbal/:token', function (req, res) {
     try {
-        let j = jwt.verify(req.params.token, config.jwt_secret);
-        let uid = j.uid;
-        sqlite_connector.getBal(uid)
+        let obj = jwt.verify(req.params.token, config.jwt_secret);
+        let requiredKeys = ["uid"];
+        let keys = Object.keys(obj).sort();
+        if (keys !== requiredKeys) {
+            throw new Error("Invalid properties specified. ");
+        }
+        sqlite_connector.getBal(obj.uid)
             .then((bal) => {
                 res.end(JSON.stringify({
-                    balance: bal
+                    balance: bal,
+                    success: true
                 }));
-            })
+            });
     } catch (e) {
-        res.end("{\"error\": \"Your token was not valid\"}");
-        console.log(e);
+        let errormsg = {
+            error: "Invalid token. ",
+            success: false
+        };
+        if (e.message !== null)
+            errormsg.error = e.message;
+        res.end(JSON.stringify(errormsg));
+        console.error(errormsg);
     }
 });
 
 app.use('/setbal/:token', function (req, res) {
     try {
-        let j = jwt.verify(req.params.token, config.jwt_secret);
-        let uid = j.uid;
-        sqlite_connector.updateBal(uid, j.amount)
+        let obj = jwt.verify(req.params.token, config.jwt_secret);
+        let requiredKeys = ["uid", "amount"];
+        let keys = Object.keys(obj).sort();
+        if (keys !== requiredKeys) {
+            throw new Error("Invalid properties specified. ");
+        }
+        sqlite_connector.updateBal(obj.uid, obj.amount)
             .then(() => {
                 res.end(JSON.stringify({
                     success: true
-                }))
-            })
+                }));
+            });
     } catch (e) {
-        res.end("{\"error\": \"Your token was not valid\"}")
+        let errormsg = {
+            error: "Invalid token. ",
+            success: false
+        };
+        if (e.message !== null)
+            errormsg.error = e.message;
+        res.end(JSON.stringify(errormsg));
+        console.error(errormsg);
     }
 });
 
 app.use('/addtransaction/:token', function (req, res) {
     try {
-        let j = jwt.verify(req.params.token, config.jwt_secret);
-        let uid = j.uid;
-        let amount = j.amount;
-        let reason = j.reason;
-        sqlite_connector.addTransaction(uid, amount, reason)
+        let obj = jwt.verify(req.params.token, config.jwt_secret);
+        let requiredKeys = ["uid", "amount", "reason"];
+        let keys = Object.keys(obj).sort();
+        if (keys !== requiredKeys) {
+            throw new Error("Invalid properties specified. ");
+        }
+        sqlite_connector.addTransaction(obj.uid, obj.amount, obj.reason)
             .then(() => {
                 res.end(JSON.stringify({
                     success: true
-                }))
-            })
+                }));
+            });
     } catch (e) {
-        res.end("{\"error\": \"Your token was not valid\"}")
+        let errormsg = {
+            error: "Invalid token. ",
+            success: false
+        };
+        if (e.message !== null)
+            errormsg.error = e.message;
+        res.end(JSON.stringify(errormsg));
+        console.error(errormsg);
     }
 });
 
 app.use('/getguildxp/:token', function (req, res) {
     try {
-        let j = jwt.verify(req.params.token, config.jwt_secret);
-        let uid = j.uid;
-        let guildId = j.guildId;
-        sqlite_connector.getGuildXp(uid, guildId)
+        let obj = jwt.verify(req.params.token, config.jwt_secret);
+        let requiredKeys = ["uid", "guildId"];
+        let keys = Object.keys(obj).sort();
+        if (keys !== requiredKeys) {
+            throw new Error("Invalid properties specified. ");
+        }
+        sqlite_connector.getGuildXp(obj.uid, obj.guildId)
             .then((xp) => {
                 res.end(JSON.stringify({
                     guildxp: xp[0],
                     awardedxp: xp[1],
-                    totalxp: xp[0]+xp[1]
-                }))
-            })
+                    totalxp: xp[0] + xp[1],
+                    success: true
+                }));
+            });
     } catch (e) {
-        res.end("{\"error\": \"Your token was not valid\"}")
+        let errormsg = {
+            error: "Invalid token. ",
+            success: false
+        };
+        if (e.message !== null)
+            errormsg.error = e.message;
+        res.end(JSON.stringify(errormsg));
+        console.error(errormsg);
     }
 });
 
-app.listen(config.port, () => console.log("NadekoConnector listening at port "+config.port+"."));
+app.use('/updateguildxp/:token', function (req, res) {
+    try {
+        let obj = jwt.verify(req.params.token, config.jwt_secret);
+        let requiredKeys = ["uid", "guildId", "amount"].sort();
+        let keys = Object.keys(obj).sort();
+        if (keys !== requiredKeys) {
+            throw new Error("Invalid properties specified. ");
+        }
+        sqlite_connector.updateGuildXp(obj.uid, obj.guildId, obj.amount)
+            .then(() => {
+                res.end(JSON.stringify({
+                    success: true
+                }));
+            });
+    } catch (e) {
+        let errormsg = {
+            error: "Invalid token. ",
+            success: false
+        };
+        if (e.message !== null)
+            errormsg.error = e.message;
+        res.end(JSON.stringify(errormsg));
+        console.error(errormsg);
+    }
+});
+
+app.listen(config.port, () => console.log("NadekoConnector listening at port " + config.port + "."));
