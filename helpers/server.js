b047ed5f5635f92cfabf3e9c5e405db94efe7db8
rjt <rjt.rockx@gmail.com>
2019-02-11 21:59:25 +0530

    Remove unnecessary async code

--- helpers/server.js
+++ helpers/server.js
@@ -6,12 +6,11 @@ const isPortAvailable = require("is-port-available");
 const jwt = require("jsonwebtoken");
 
 const Connector = require("./connector.js");
-const fileManager = require("./fileManager.js");
-
+const File = require("./file.js");
 
 class server {
 	constructor(settings) {
-		if (!settings || typeof settings !== "object")
+		if (!(settings instanceof Object))
 			throw new Error("Invalid settings specified.");
 
 		const requiredSettingsModel = {
@@ -27,63 +26,51 @@ class server {
 			this[property] = settings[property];
 		}
 
-		if (!Array.isArray(settings.disabledEndpoints))
+		if (!(settings.disabledEndpoints instanceof Array))
 			settings.disabledEndpoints = [];
 		this.disabledEndpoints = settings.disabledEndpoints;
-
-		if (typeof settings.readOnly !== "boolean")
-			settings.readOnly = false;
-		this.readOnly = settings.readOnly;
+		this.readOnly = Boolean(settings.readOnly);
+		this._initialized = false;
 	}
 
 	async initialize() {
-		if (!(await isPortAvailable(this._port)))
+		if (!(await isPortAvailable(this.port)))
 			throw new Error("Port unavailable.");
 
-		if (!(await fileManager.checkIfExists(this.credentialsPath)))
-			throw new Error("Credentials does not exist.");
-		if (!(await fileManager.checkReadable(this.credentialsPath)))
-			throw new Error("Credentials is not readable.");
+		const credentials = new File(this.credentialsPath);
+		if (!credentials.exists) throw new Error("Credentials does not exist.");
+		if (!credentials.readable) throw new Error("Credentials is not readable.");
 
-		if (!(await fileManager.checkIfExists(this.databasePath)))
-			throw new Error("Database does not exist.");
-		if (!(await fileManager.checkReadable(this.databasePath)))
-			throw new Error("Database is not readable.");
-
-		if (!(await fileManager.checkWritable(this.databasePath)))
-			this.readOnly = true;
+		const database = new File(this.databasePath);
+		if (!database.exists) throw new Error("Database does not exist.");
+		if (!database.readable) throw new Error("Database is not readable.");
+		if (!database.writable) this.readOnly = true;
 
 		this.connector = new Connector(this.databasePath, this.credentialsPath, this.disabledEndpoints, this.readOnly);
-		await this.connector.initialize();
+		this.connector.initialize();
 
 		this.app = express();
 		this.app.use(helmet());
 		this.app.use(morgan("dev"));
 
-		this.connector.endpoints.map(this.registerEndpoint);
+		this.connector.endpoints.forEach(endpoint => this.registerEndpoint(endpoint));
 
-		this.app.listen(this._port);
+		this.app.listen(this.port);
+		this._initialized = true;
+		return this;
 	}
 
-	get port() {
-		return this._port;
-	}
-
-	set port(port) {
-		this._port = port;
-	}
-
-	async registerEndpoint(endpoint) {
+	registerEndpoint(endpoint) {
 		if (!this.connector)
 			throw new Error("Server not initialized.");
 		if (typeof endpoint !== "string")
 			throw new Error("Invalid endpoint specified.");
-		this.app.get(`/${endpoint.toLowerCase()}/:token`, async (request, response) => {
+		this.app.get(`/${endpoint.toLowerCase()}/:token`, (request, response) => {
 			let result = {};
 			try {
-				const query = await this.parseToken(request.params.token);
-				const properties = await this.checkProperties(query, endpoint);
-				result = await this.handleEndpoint(endpoint, query, properties);
+				const query = this.parseToken(request.params.token);
+				const properties = this.checkProperties(query, endpoint);
+				result = this.handleEndpoint(endpoint, query, properties);
 			}
 			catch (error) {
 				result = { error: error.name, message: error.message };
@@ -94,24 +81,24 @@ class server {
 		});
 	}
 
-	async parseToken(token) {
-		const query = await jwt.verify(token, this.password);
+	parseToken(token) {
+		const query = jwt.verify(token, this.password);
 		if (!query || typeof query !== "object")
 			throw new Error("Invalid token.");
 		return query;
 	}
 
-	async checkProperties(query, endpoint) {
+	checkProperties(query, endpoint) {
 		const keys = Object.keys(query).sort();
-		const requiredProperties = await this.getRequiredProperties(endpoint);
-		Object.keys(requiredProperties).map(property => {
+		const requiredProperties = this.getRequiredProperties(endpoint);
+		for (const property of Object.keys(requiredProperties)) {
 			if (!(keys.includes(property) && typeof query[property] === requiredProperties[property].type))
 				throw new Error(`Invalid properties specified. \n${requiredProperties[property].error}`);
-		});
+		}
 		return Object.keys(requiredProperties);
 	}
 
-	async getRequiredProperties(endpoint) {
+	getRequiredProperties(endpoint) {
 		switch (endpoint) {
 			case "getBotInfo":
 			case "getTables":
@@ -287,10 +274,8 @@ class server {
 		}
 	}
 
-	async handleEndpoint(endpoint, query, properties) {
-		const args = [];
-		properties.map(property => args.push(query[property]));
-		return this.connector[endpoint](...args);
+	handleEndpoint(endpoint, query, properties) {
+		return this.connector[endpoint](...properties.map(property => query[property]));
 	}
 }
 
