c7a0244f850fc5a0c518f90ab2b34a7ff23834a6
rjt <rjt.rockx@gmail.com>
2019-02-11 21:59:36 +0530

    Cleanup code

--- main.js
+++ main.js
@@ -3,16 +3,9 @@ const server = require("./helpers/server.js");
 const pkg = require("./package.json");
 
 const startServers = async () => {
-	const servers = [], ports = [];
-	await Promise.all(config.bots.map(async function (bot) {
-		const botServer = new server(bot);
-		await botServer.initialize();
-		servers.push(botServer);
-		ports.push(botServer.port);
-	}));
-	console.log(`NadekoConnector ${pkg.version}`);
-	console.log(`${pkg.description}`);
-	console.log(`Listening on port${ports.length<2?"":"s"} ${ports.join(", ")}.`);
+	const servers = await Promise.all(config.bots.map(bot => new server(bot).initialize()));
+	console.log(`NadekoConnector ${pkg.version}\n${pkg.description}`);
+	console.log(`Listening on port${servers.map(s => s.port).length < 2 ? "" : "s"} ${servers.map(s => s.port).join(", ")}.`);
 };
 
 startServers();
